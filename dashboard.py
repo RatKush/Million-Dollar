498f8bf71ae93aa2db467c1b07d165f5429f7b34
Ratnesh Kushwaha <89016010+RatKush@users.noreply.github.com>
2025-10-01 21:40:37 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -2116,5 +2116,5 @@ def get_free_port(preferred_port, fallback_port):
 # ------------------------------------------------
 if __name__ == '__main__':
     #app.run(debug= False, host='0.0.0.0', port=8050) #for live hosted version  https://million-dollar.onrender.com/
-    #app.run(debug= True) #self
-    app.run(debug=False, port=get_free_port(8050, 8060))  #for download 
+    app.run(debug= True) #self
+    #app.run(debug=False, port=get_free_port(8050, 8060))  #for download 
