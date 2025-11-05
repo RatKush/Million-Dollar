4ac7c772e8db38a22a70f329d902d8bc628b7412
Ratnesh Kushwaha <89016010+RatKush@users.noreply.github.com>
2025-11-05 18:32:39 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1525,7 +1525,8 @@ def warning_plot(warning):
 # MAIN
 # ------------------------------------------------
 if __name__ == '__main__':
-    #app.run(debug= False, host='0.0.0.0', port=8050) #for live hosted version  https://million-dollar.onrender.com/
-    app.run(debug= True) #self
+    app.run(debug= False, host='0.0.0.0', port=8050) #for live hosted version  https://million-dollar.onrender.com/
+    #app.run(debug= True) #self
     #app.run(debug=False, port=get_free_port(8050, 8060))  #for download 
+
 
